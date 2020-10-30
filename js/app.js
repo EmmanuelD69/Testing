f9c4cb615813dfc34b5ad967fd8146d1320396fd
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-10-30 21:16:49 +0100

    Js building test function to select targets

--- js/app.js
+++ js/app.js
@@ -5,9 +5,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const links = document.querySelectorAll("a");
 
 links.forEach(function(link){
+  // console.log(link)
   link.addEventListener("click", test)
 })
 
-function test () {
-  console.log("test");
+function test (e) {
+  // console.log(e.target.className);
+  switch (e.target.className) {
+    case "link1":
+      console.log("section 1")
+      break
+    case "link2":
+      console.log("section 2")
+      break
+    case "link3":
+      console.log("section 3")
+      break
+    case "scrollTop":
+      console.log("back to the top")
+      break
+  }
 }
