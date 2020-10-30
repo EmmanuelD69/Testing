cb122150d2a0d095d514f836deaf45140a1057f3
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-10-30 20:58:41 +0100

    Javascript querySelectorAll practice

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM entièrement chargé et analysé");
   });
+
+const links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+  link.addEventListener("click", test)
+})
+
+function test () {
+  console.log("test");
+}
